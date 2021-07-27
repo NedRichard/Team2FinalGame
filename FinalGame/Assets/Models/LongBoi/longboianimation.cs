55308e6dec4501d9e7af0805ab97b35503f1bbc2
Liisa <liisa.pirhonen@gmail.com>
2021-07-27 15:15:06 +0300

    Lighting, setting colliders, fuse box

---
+++ FinalGame/Assets/Models/LongBoi/longboianimation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class longboianimation : MonoBehaviour
+{
+    public Animator animator;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
