7d42c749044f2b049c7c4c90f9c86d3e0db689dd
Liisa <liisa.pirhonen@gmail.com>
2021-07-28 23:07:26 +0300

    UV fixes, running animation, main menu
    Worked on Classroom 1, exit sign, adding stuff, doorways fixed, main menu tweaking, running animation works but needs the speed added .

---
+++ FinalGame/Assets/Models/LongBoi/LiisaTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LiisaTest : MonoBehaviour
+{
+    public NavMeshAgent navMesh;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponentInParent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(navMesh.speed <= 8)
+        {
+            anim.SetFloat("Speed", 7.5f);
+        } 
+        else if(navMesh.speed >= 8)
+        {
+            anim.SetFloat("Speed", 8.5f);
+        }
+    }
+}
